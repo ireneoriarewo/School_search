c2db247dfca46d097df99e859b525a2587dfe4f6
Irene Oriarewo <81082912+ireneoriarewo@users.noreply.github.com>
2022-09-28 22:48:40 +0100

    Add files via upload

---
+++ my_data.py
@@ -0,0 +1,27 @@
+agriculture = ['Agriculture', 'Animal Sciences', 'Forestry','Horticulture and Crop Science', 'Marine Studies']
+
+applied_science = ['Emergency & Disaster Management', 'Family & Consumer Science', 'Fashion, Textiles and Luxury Goods', 'Food Sciences', 'Forensic Science', 'Library Science', 'Military Science', 'Museum Studies', 'Real Estate & Property Management', 'Social Work',
+]
+
+
+arts =['Architecture', 'Art History', 'Design', 'Fashion Design', 'Film, Photography & Media', 'Fine Arts', 'Graphic Design', 'Industrial Design', 'Interior Design', 'Landscape Architecture', 'Music', 'Music Composition', 'Music Performance', 'Musicology', 'Theatre & Dance', 'Urban Planning', 'Visual Arts',
+]
+
+
+business = ['Accounting', 'Actuarial Science', 'Agribusiness', 'Auditing', 'Banking & Investment', 'Business Administration', 'Business Intelligence & Analytics', 'Commerce', 'Construction Management', 'Corporate Communication', 'Corporate Social Responsibility', 'Digital Communication', 'Digital Marketing', 'Engineering Management', 'Entrepreneurship', 'Executive MBA', 'Finance', 'Forensic Accounting', 'Human Resource Management', 'Innovation Management', 'International Business', 'IT Management', 'Leadership', 'Management of Creative Industries', 'Management Studies', 'Marketing', 'Marketing Management', 'Master in Business Administration (MBA)', 'Master in Management (MIM)', 'Operations and Quality Management', 'Project Management', 'Public Administration', 'Retail Management', 'Risk Management', 'Strategic Management', 'Supply Chain Management & Logistics', 'Taxation', 'Transport Management',
+            ]
+
+computer = ['Artificial Intelligence','Business Information Systems', 'Computer Sciences','Cyber Security','Data Science & Big Data','Geographical Information Systems (GIS)','Health Informatics','Human Computer Interaction','Informatics & Information Technology','IT Security','Machine Learning','Software Engineering','User Experience Design','Video Games & Animation','Web Technologies & Cloud Computing',
+]
+
+engineering = ['Aerospace Engineering','Automotive Engineering','Bio & Biomedical Engineering','Chemical Engineering','Civil Engineering & Construction','Communications Engineering','Electrical Engineering','Electronics & Embedded Technology','Energy & Power Engineering','Environmental Engineering','General Engineering & Technology','Industrial & Systems Engineering','Marine Engineering','Materials Science & Engineering','Mechanical Engineering','Mechatronics','Mining, Oil & Gas','Nuclear Engineering','Robotics','Sound Engineering','Sustainable Energy','Transportation Engineering',
+]
+
+law = ['Business Law','Civil & Private Law','Criminal Justice','Criminal Law','European Law','International Law','Legal Studies','Master of Laws (LLM)','Patent & Intellectual Property Law','Public Law',
+]
+
+medicine = ['Biomedicine','Clinical Psychology','Complementary & Alternative Medicine','Dentistry','Epidemiology','Gerontology','Health Sciences','Healthcare Administration','Human Medicine','Immunology','Kinesiology','Medical Imaging','Midwifery','Nursing','Nutrition & Dietetics','Occupational Health and Safety','Pharmacy','Physiology','Physiotherapy','Public Health','Speech Pathology','Veterinary Medicine',
+]
+expanded_list = [agriculture, applied_science, arts, business, computer, engineering, law, medicine]
+
+discipline  = ['Agriculture & Forestry', 'Applied Sciences & Professions', 'Arts, Design & Architecture', 'Business & Management', 'Computer Science & IT', 'Engineering & Technology', 'Law', 'Medicine & Health']
